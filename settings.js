10a575832ade613486b342a57912a644b9a59ade
ablaye2020 <163043278+ablaye2020@users.noreply.github.com>
2026-04-17 02:56:45 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,7 @@
-// settings.js
+// settings.js - 𝐙𝐄𝐓𝐒𝐔-MD
 export default {
-  SESSION_ID: "" // add you session id
+  SESSION_ID: "", // Ajoute ton session ID ici
+  BOT_NAME: "𝐙𝐄𝐓𝐒𝐔-MD",
+  OWNER_NAME: "JOYBOY",
+  CITATION: '"Le respect ne se demande pas"'
 };
